da9abd7ffc1ccc421036b91b7e39b1a54e9a8c71
mahaltrupti <truptimahal2004@gmail.com>
2025-08-30 19:23:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+// Example JS to show how you can access the selector class
+document.querySelectorAll(".selector").forEach((el, index) => {
+  console.log(`Paragraph with selector class #${index + 1}:`, el.textContent);
+});
